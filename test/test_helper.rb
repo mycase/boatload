5772e5d0aae6fe574d36ae8c10dfa4e6ed205493
Collin Styles <collingstyles@gmail.com>
2020-08-02 08:55:09 -0700

    Move importing of `logger` into test helper
    The import was missing from `AsyncBatchProcessorTest` but rather than
    add it there, let's just put it in the test helper file since all the
    tests need it anyway.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'boatload'
 
+require 'logger'
 require 'minitest/autorun'
 require 'mocha/minitest'
 require 'shoulda-context'
--- test/worker_test.rb
+++ test/worker_test.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'logger'
 require 'test_helper'
 
 module Boatload
